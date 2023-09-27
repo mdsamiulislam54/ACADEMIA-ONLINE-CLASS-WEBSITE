fde0f907f66ae26cbe53e82ef946b6378f4633f7
mdsamiulislam54 <samiulm5332@gmail.com>
2023-09-28 01:09:11 +0600

    new css and html and javascript code add tiny slider add

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+let valuDisplay = document.querySelectorAll(".count");
+let interval = 4000;
+valuDisplay.forEach((valuDisplay) => {
+    let startvalue = 0;
+    let endvalue = parseInt(valuDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endvalue);
+    let counter = setInterval(function(){
+        startvalue +=1;
+        valuDisplay.textContent = startvalue;
+        if(startvalue == endvalue){
+            clearInterval(counter);
+        }
+
+    } , 10);
+
+});
+var slider = tns({
+    container: '.my-courese',
+    items: 3,
+    slideBy: 'page',
+    autoplay: true,
+    controls:false,
+    arrowKeys: true,
+  });
+
